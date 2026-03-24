9026f106f69f2987755e24d352213020174d11d1
ToadMonarch <macq1300@gmail.com>
2026-03-24 12:28:56 +1100

    Character Movement - Click to Move
    Click to Move

--- game-4-good-rpg/Scripts/character_body_2d.gd
+++ game-4-good-rpg/Scripts/character_body_2d.gd
@@ -1,25 +1,21 @@
 extends CharacterBody2D
 
+@export var speed := 200
+var target_position: Vector2
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+func _ready():
+	target_position = global_position
 
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		target_position = get_global_mouse_position()
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func _physics_process(delta):
+	var direction = (target_position - global_position).normalized()
+	
+	if global_position.distance_to(target_position) > 5:
+		velocity = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity = Vector2.ZERO
+	
 	move_and_slide()
