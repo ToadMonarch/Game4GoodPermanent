668be0fdb74cb3b2402a8877f77ced06f0598a01
StevenNguyen2003 <minhtien.nguyen@student.uts.edu.au>
2026-05-14 16:57:14 +1000

    Integrate with setting

--- game-4-good-rpg/Chapter 1/MenuScript.gd
+++ game-4-good-rpg/Chapter 1/MenuScript.gd
@@ -4,11 +4,14 @@ extends CanvasLayer
 @onready var settings_menu = $SettingsMenu
 @onready var volume_slider = $SettingsMenu/VolumeSlider
 @onready var music_player = $"../MusicPlayer"
-@onready var player = $"../Player"
+var player: Node2D
 
 var menu_open := false
 
 func _ready():
+	player = get_parent().get_node_or_null("Player") as Node2D
+	if player == null:
+		player = get_parent().get_node_or_null("CharacterBody2D") as Node2D
 	# Start with menu hidden
 	settings_menu.visible = false
 	load_settings()
@@ -37,13 +40,15 @@ func _on_close_pressed():
 
 
 func _on_change_skin_pressed() -> void:
-	player.cycle_skin()
+	if player:
+		player.cycle_skin()
 
 
 func _on_save_pressed() -> void:
 	var config = ConfigFile.new()
 	config.set_value("audio", "volume", volume_slider.value)
-	config.set_value("player", "skin", player.current_skin_index)
+	if player:
+		config.set_value("player", "skin", player.current_skin_index)
 	config.save("user://settings.cfg")
 	
 func load_settings():
@@ -53,5 +58,6 @@ func load_settings():
 		var skin = config.get_value("player", "skin", 0)
 		
 		volume_slider.value = vol
-		player.current_skin_index = skin
-		player.set_skin(player.SKIN_ORDER[skin])
+		if player:
+			player.current_skin_index = skin
+			player.set_skin(player.SKIN_ORDER[skin])
